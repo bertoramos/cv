0359f106f3efdecf2c27eb7d291e9996ef49ba67
bertoramos <bertoramos@users.noreply.github.com>
2024-12-05 16:34:39 +0000

    deploy: 06e671c1cddb133a93c5017724778926d5096c7a

--- assets/ios.transition-legacy-rVxzQud7.js
+++ assets/ios.transition-legacy-rVxzQud7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-o_P3UmnU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-egK4X3oq.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-BzaFJPsR.js
+++ assets/md.transition-legacy-BzaFJPsR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-o_P3UmnU.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-egK4X3oq.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
