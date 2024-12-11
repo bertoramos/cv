848f02dff27037b33cfff48c098cbf542c2def65
bertoramos <bertoramos@users.noreply.github.com>
2024-12-11 14:33:46 +0000

    deploy: 22b04c92fca8af486cb27a6b1d35217197b06afe

--- assets/ios.transition-legacy-BDO3Z4Cq.js
+++ assets/ios.transition-legacy-BDO3Z4Cq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-98Po-IOT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-_dAq2CL5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-CCZXTFH3.js
+++ assets/md.transition-legacy-CCZXTFH3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-98Po-IOT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-_dAq2CL5.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
