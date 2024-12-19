7b08185d75b771e61e6c6dd9cb2816ffc09b6717
bertoramos <bertoramos@users.noreply.github.com>
2024-12-19 13:42:47 +0000

    deploy: 63b3cbe4e626afb55a426dcccef308fa36d8888c

--- assets/ios.transition-legacy-hyAcmAvB.js
+++ assets/ios.transition-legacy-hyAcmAvB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C-Bp6ZQe.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-ybwwEHa2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
