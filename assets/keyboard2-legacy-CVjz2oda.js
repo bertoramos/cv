3dba2c81b9a0309acb8663de9582bdf93d46a98b
bertoramos <bertoramos@users.noreply.github.com>
2024-12-23 15:14:49 +0000

    deploy: 578c9f75ba6c3f28d0e0d31cc74828e85295d675

--- assets/keyboard2-legacy-CVjz2oda.js
+++ assets/keyboard2-legacy-CVjz2oda.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DH_teqzi.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Due41yEX.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
