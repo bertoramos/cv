cae331b0e38394b09d32336a4ac63fc610d7bc82
bertoramos <ramossanchezalberto@gmail.com>
2024-12-24 17:53:30 +0000

    feat(education page): add props to education card component

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -7,19 +7,25 @@ import { globe, location, logoAndroid } from 'ionicons/icons';
 
 interface EducationComponentProps {
     title: string;
-    company: string;
-    time: string;
+    corporation: string;
+    from: string;
+    to: string;
+    place: string;
+    web: string;
+    logo: string;
     description: string;
-    technologies: string[];
-    image: string;
+    technologies: {
+        name: string;
+        logo: any;
+    }[]
 }
 
-const EducationComponent: React.FC<EducationComponentProps> = ({ title, company, time, description, technologies, image }) => {
+const EducationComponent: React.FC<EducationComponentProps> = ( props ) => {
     return (
         <IonAccordion>
             <IonItem slot="header" color="light">
-                <IonLabel className='ion-text-start'>Title @ Corporation</IonLabel>
-                <IonLabel className='ion-text-end'>From - To</IonLabel>
+                <IonLabel className='ion-text-start'>{props.title} @ {props.corporation}</IonLabel>
+                <IonLabel className='ion-text-end'>{props.from} - {props.to}</IonLabel>
             </IonItem>
             <div className="ion-padding" slot="content">
                 <IonCard>
@@ -27,10 +33,10 @@ const EducationComponent: React.FC<EducationComponentProps> = ({ title, company,
                     <IonCardHeader>
                         <IonRow>
                             <IonCol size="auto">
-                                <IonIcon icon={location} /> Place
+                                <IonIcon icon={location} /> {props.place}
                             </IonCol>
                             <IonCol size="auto">
-                                <IonIcon icon={globe} /> Web
+                                <IonIcon icon={globe} /> {props.web}
                             </IonCol>
                         </IonRow>
                     </IonCardHeader>
@@ -38,19 +44,19 @@ const EducationComponent: React.FC<EducationComponentProps> = ({ title, company,
                     <IonCardContent>
                         <IonRow>
                             <IonCol offset="0" size="12" size-md="2" push-md="9.5" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                <img style={{ margin: "auto" }} src="https://picsum.photos/100" alt="" />
+                                <img style={{ margin: "auto" }} src={props.logo} alt="" />
                             </IonCol>
                             <IonCol offset="0.5" size="12" size-md="9" pull-md="2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore.
+                                {props.description}
                             </IonCol>
                         </IonRow>
                         <IonRow>
                             {
-                                technologies.map((tech, index) => (
+                                props.technologies.map((tech, index) => (
                                     <IonCol key={index} size="auto">
                                         <IonChip>
-                                            <IonIcon icon={logoAndroid} color="primary"></IonIcon>
-                                            <IonLabel>{tech}</IonLabel>
+                                            <IonIcon icon={tech.logo} color="primary"></IonIcon>
+                                            <IonLabel>{tech.name}</IonLabel>
                                         </IonChip>
                                     </IonCol>
                                 ))
@@ -69,20 +75,32 @@ const Education: React.FC = () => {
             <IonTitle className="ion-text-center ion-padding" style={{fontSize: "4vh"}}>Education</IonTitle>
             <IonAccordionGroup expand="inset">
                 <EducationComponent
-                    title={''}
-                    company={''}
-                    time={''}
-                    description={''}
-                    technologies={["tech1", "tech2"]} 
-                    image={''}
+                    title={'Title1'}
+                    corporation={'Corporation1'}
+                    from={'From1'}
+                    to={'To1'}
+                    place={'Place1'}
+                    web={'Web1.com'}
+                    logo={'https://picsum.photos/200'}
+                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore.'}
+                    technologies={
+                        [{name: "tech11", logo: logoAndroid},
+                        {name: "tech12", logo: logoAndroid}]
+                    }                    
                 />
                 <EducationComponent
-                    title={''}
-                    company={''}
-                    time={''}
-                    description={''}
-                    technologies={["tech1", "tech2"]} 
-                    image={''}
+                    title={'Title2'}
+                    corporation={'Corporation2'}
+                    from={'From2'}
+                    to={'To2'}
+                    place={'Place2'}
+                    web={'Web2.com'}
+                    logo={'https://picsum.photos/200'}
+                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore. Grade: 10'}
+                    technologies={
+                        [{name: "tech21", logo: logoAndroid},
+                        {name: "tech22", logo: logoAndroid}]
+                    }     
                 />
             </IonAccordionGroup>
         </IonContent>
