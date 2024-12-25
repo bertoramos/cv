ac90d3ae02e856a8b100d2b75d1b5268b894c7b1
bertoramos <bertoramos@users.noreply.github.com>
2024-12-25 23:39:24 +0000

    deploy: 14c6edd6d05135fd7781bb4b2527229c4c6369d5

--- assets/md.transition-legacy-n6eqlZsw.js
+++ assets/md.transition-legacy-n6eqlZsw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BFAow92U.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bn1sPKui.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-CwJEVHK6.js
+++ assets/swipe-back-legacy-CwJEVHK6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BFAow92U.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Bn1sPKui.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
