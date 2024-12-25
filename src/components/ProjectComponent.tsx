14c6edd6d05135fd7781bb4b2527229c4c6369d5
bertoramos <ramossanchezalberto@gmail.com>
2024-12-25 23:38:01 +0000

    feat(project): project detail modal content

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -1,10 +1,18 @@
 import React, { useRef } from "react";
-import { IonButton, IonButtons, IonChip, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonModal, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonChip, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonModal, IonRow, IonText, IonTitle, IonToolbar } from "@ionic/react";
 import "./ProjectComponent.css";
 import { mailOutline, logoGithub, logoLinkedin } from "ionicons/icons";
 
 interface ProjectDetailProps {
-    trigger: string;
+    modal_trigger: string;
+    title: string;
+    image: string;
+    description: string;
+    url: string;
+    technologies: {
+        name: string;
+        logo: any;
+    }[];
 };
 
 const ProjectDetail: React.FC<ProjectDetailProps> = (props) => {
@@ -15,7 +23,7 @@ const ProjectDetail: React.FC<ProjectDetailProps> = (props) => {
     };
 
     return (
-        <IonModal className="project-detail-modal" ref={modal} trigger={props.trigger}>
+        <IonModal className="project-detail-modal" ref={modal} trigger={props.modal_trigger}>
             <IonHeader style={{ "--background": "#54d61c", "--ion-background-color": "red" }}>
                 <IonToolbar>
                     <IonTitle></IonTitle>
@@ -25,7 +33,38 @@ const ProjectDetail: React.FC<ProjectDetailProps> = (props) => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="project-detail-modal-content ion-padding">
-                Project details
+                <IonGrid>
+                    <IonRow>
+                        <IonCol size="12" size-md="6">
+                            <IonTitle className="ion-text-center ion-padding-bottom">
+                                {props.title}
+                            </IonTitle>
+                            <IonText className="ion-text-justify ion-padding-bottom">
+                                {props.description}
+                            </IonText>
+                            <div className="ion-padding">
+                                <a 
+                                    href={props.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer">Click here for more info.
+                                </a>
+                            </div>
+                            <div>
+                                {
+                                    props.technologies.map((tech, index) => (
+                                        <IonChip color="primary" key={index}>
+                                            <IonIcon color="light" icon={tech.logo}></IonIcon>
+                                            <IonLabel color="light">{tech.name}</IonLabel>
+                                        </IonChip>
+                                    ))
+                                }
+                            </div>
+                        </IonCol>
+                        <IonCol size="12" size-md="6" className="ion-text-center">
+                            <img style={{height: "50%"}} src={props.image} alt={props.title} />
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
             </IonContent>
         </IonModal>
     );
@@ -34,6 +73,8 @@ const ProjectDetail: React.FC<ProjectDetailProps> = (props) => {
 interface ProjectComponentProps {
     image: string;
     title: string;
+    short_description: string;
+    url: string;
     description: string;
     technologies: {
         name: string;
@@ -59,15 +100,20 @@ const ProjectComponent: React.FC<ProjectComponentProps> = (props) => {
                     className="project-component-text-column project-component-center-column">
                     <p className="project-component-title">{props.title}</p>
                     <div className="project-component-description">
-                        <div className="ion-padding-bottom project-component-description-text">{props.description}</div>
+                        <div className="ion-padding-bottom project-component-description-text">{props.short_description}</div>
                         <IonButton
                             id={props.title + "-open-project-detail-modal"}
                             fill="clear"
                             className="project-component-detail-button">
                             Details
                         </IonButton>
-                        <ProjectDetail 
-                            trigger={props.title + "-open-project-detail-modal"}
+                        <ProjectDetail
+                            modal_trigger={props.title + "-open-project-detail-modal"}
+                            title={props.title}
+                            image={props.image}
+                            description={props.description}
+                            url={props.url}
+                            technologies={props.technologies}
                         />
                         <div>
                             {
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,19 +8,21 @@ import { logoAndroid } from 'ionicons/icons';
 const Projects: React.FC = () => {
 
     var lorem_text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore.';
-
+    var short_lorem_text = 'Lorem ipsum dolor sit amet consectetur';
     return (
         <IonContent color="dark">
             <div className="project-snap-container">
-
+                
                 <div className="project-snap-section">
                     <ProjectComponent
-                        title={'Project1'}
+                        title={'Projectó 1 #@#€~@'}
                         description={lorem_text}
+                        short_description={short_lorem_text}
+                        url={'http://google.com'}
                         image={'https://picsum.photos/800'}
                         technologies={[
-                            {name: "tech11", logo: logoAndroid},
-                            {name: "tech12", logo: logoAndroid}
+                            { name: "tech41", logo: logoAndroid },
+                            { name: "tech42", logo: logoAndroid }
                         ]}
                     />
                 </div>
@@ -29,10 +31,12 @@ const Projects: React.FC = () => {
                     <ProjectComponent
                         title={'Project2'}
                         description={lorem_text}
+                        short_description={short_lorem_text}
+                        url={'http://google.com'}
                         image={'https://picsum.photos/800'}
                         technologies={[
-                            {name: "tech21", logo: logoAndroid},
-                            {name: "tech22", logo: logoAndroid}
+                            { name: "tech41", logo: logoAndroid },
+                            { name: "tech42", logo: logoAndroid }
                         ]}
                     />
                 </div>
@@ -41,10 +45,12 @@ const Projects: React.FC = () => {
                     <ProjectComponent
                         title={'Project3'}
                         description={lorem_text}
+                        short_description={short_lorem_text}
+                        url={'http://google.com'}
                         image={'https://picsum.photos/800'}
                         technologies={[
-                            {name: "tech31", logo: logoAndroid},
-                            {name: "tech32", logo: logoAndroid}
+                            { name: "tech41", logo: logoAndroid },
+                            { name: "tech42", logo: logoAndroid }
                         ]}
                     />
                 </div>
@@ -53,10 +59,12 @@ const Projects: React.FC = () => {
                     <ProjectComponent
                         title={'Project4'}
                         description={lorem_text}
+                        short_description={short_lorem_text}
+                        url={'http://google.com'}
                         image={'https://picsum.photos/800'}
                         technologies={[
-                            {name: "tech41", logo: logoAndroid},
-                            {name: "tech42", logo: logoAndroid}
+                            { name: "tech41", logo: logoAndroid },
+                            { name: "tech42", logo: logoAndroid }
                         ]}
                     />
                 </div>
