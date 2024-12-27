b9f40b5b21cf025e21527a9145ea3e84f837ee46
bertoramos <bertoramos@users.noreply.github.com>
2024-12-27 20:41:25 +0000

    deploy: daccc072da4f7b74756a859382111732dcde065a

--- assets/md.transition-legacy-ClxtnEW5.js
+++ assets/md.transition-legacy-ClxtnEW5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dk4OKWaj.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CPfJ_xFK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
