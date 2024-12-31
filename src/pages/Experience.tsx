3c37406e234141ecd93a44296fc393e00229e798
bertoramos <ramossanchezalberto@gmail.com>
2024-12-31 21:02:07 +0000

    feat(experience): add experience content

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,7 +1,7 @@
 
 import { IonAccordion, IonAccordionGroup, IonButton, IonCard, IonCardContent, IonCardHeader, IonChip, IonCol, IonContent, IonIcon, IonItem, IonLabel, IonRow, IonTitle } from '@ionic/react';
 import './Experience.css';
-import { globe, location, logoAndroid } from 'ionicons/icons';
+import { globe, location, logoAndroid, logoDocker, logoHtml5, logoPython, logoRss } from 'ionicons/icons';
 
 
 
@@ -89,42 +89,65 @@ const Experience: React.FC = () => {
             <IonTitle className="ion-text-center ion-padding" style={{ fontSize: "4vh" }}>Experience</IonTitle>
             <IonAccordionGroup expand="inset">
                 <ExperienceComponent
-                    title={'Title1'}
-                    company={'Company1'}
-                    from={'From1'}
-                    to={'To1'}
-                    place={'Place1'}
-                    web={'www.web1.com'}
+                    title={'Data scientist'}
+                    company={'Fundación Universitaria de Las Palmas (FULP)'}
+                    from={'December 2023'}
+                    to={'December 2024'}
+                    place={'Las Palmas de Gran Canaria'}
+                    web={'www.fulp.es'}
                     logo={'https://picsum.photos/200'}
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore.'}
+                    description={'"Investigo" programme: "SMART GIP: Applying Artificial Intelligence (AI) to Integrated Project Management". Optimization of key processes through development through the development of a platform for payroll management and electronic invoicing.'}
                     technologies={
-                        [{ name: "tech11", logo: logoAndroid },
-                        { name: "tech12", logo: logoAndroid }]
+                        [{ name: "Python", logo: logoPython },
+                         { name: "Spacy", logo: logoRss },
+                         { name: "PHP", logo: logoRss},
+                         { name: "HTML/CSS", logo: logoHtml5}]
                     }
                     references={
                         [
-                            { name: "Reference11", url: "http://google.com" },
-                            { name: "Reference12", url: "http://google.com" }
+                            { name: "SmartGIP", url: "https://www.fulp.es/index.php/actualidad/noticias/la-fulp-investiga-la-aplicacion-de-inteligencia-artificial-la-gestion-de" },
                         ]
                     }
                 />
                 <ExperienceComponent
-                    title={'Title2'}
-                    company={'Company2'}
-                    from={'From2'}
-                    to={'To2'}
-                    place={'Place2'}
-                    web={'www.web2.com'}
+                    title={'Computer scientist'}
+                    company={'Subsea Mechatronics'}
+                    from={'Jan 2022'}
+                    to={'Jul 2023'}
+                    place={'Las Palmas de Gran Canaria'}
+                    web={'https://www.ssmrovs.com/'}
                     logo={'https://picsum.photos/200'}
-                    description={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores laborum ipsa eveniet mollitia, ex blanditiis eius totam dignissimos aspernatur suscipit, distinctio atque? Nulla ex deleniti voluptates, quas laudantium iure tempore. Grade: 10'}
+                    description={'"Investigo" programme: "IngenIoT". Improved perception of underwater environments through 3D reconstruction and analysis of data captured in campaigns.'}
                     technologies={
-                        [{ name: "tech21", logo: logoAndroid },
-                        { name: "tech22", logo: logoAndroid }]
+                        [{ name: "Python", logo: logoPython },
+                         { name: "C++", logo: logoRss },
+                         { name: "ROS1", logo: logoRss},
+                         { name: "ROS1", logo: logoRss},
+                         { name: "Docker", logo: logoDocker},
+                         { name: "NVIDIA Jetson", logo: logoRss}
+                        ]
+                    }
+                    references={
+                        []
+                    }
+                />
+                <ExperienceComponent
+                    title={'Computer scientist'}
+                    company={'IUCTC ULPGC'}
+                    from={'Sep 2019'}
+                    to={'Ene 2020'}
+                    place={'Las Palmas de Gran Canaria'}
+                    web={'https://ciberlandia.org'}
+                    logo={'https://picsum.photos/200'}
+                    description={'Computer science internships. Support in the design and development of activities for the Cyberlandia Project and the Science Week.'}
+                    technologies={
+                        [{ name: "Python", logo: logoPython },
+                         { name: "Inkscape", logo: logoRss}
+                        ]
                     }
                     references={
                         [
-                            { name: "Reference21", url: "http://google.com" },
-                            { name: "Reference22", url: "http://google.com" }
+                            {name: "Semana de la Ciencia 2019", url: "https://ciberlandia.org/semanas-de-la-ciencia-y-la-innovacion-en-canarias-2019-gran-canaria/"}
                         ]
                     }
                 />
