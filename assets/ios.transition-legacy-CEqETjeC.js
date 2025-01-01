de0cbbedf889a2112d755bacd05fbd91ee9e8aed
bertoramos <bertoramos@users.noreply.github.com>
2025-01-01 16:16:46 +0000

    deploy: 1c3e1be45556566e97a590798249a56edcd92218

--- assets/ios.transition-legacy-CEqETjeC.js
+++ assets/ios.transition-legacy-CEqETjeC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BkG9FC1-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-b4UBHCNf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
