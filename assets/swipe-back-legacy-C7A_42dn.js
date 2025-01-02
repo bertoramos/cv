b243ae7f3c7b22c21bb88efd6e396e5466b05cc2
bertoramos <bertoramos@users.noreply.github.com>
2025-01-02 17:54:39 +0000

    deploy: f8ca47439e5ef6c73ca1dc866e8a7bb8451df395

--- assets/swipe-back-legacy-C7A_42dn.js
+++ assets/swipe-back-legacy-C7A_42dn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-b4UBHCNf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DCz642y2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
