4a3c0e3ba6ce38010e27bfe8c1a9c3477c4b59a5
bertoramos <bertoramos@users.noreply.github.com>
2025-01-09 13:00:50 +0000

    deploy: de457d0c90c8356ad6c58c80040324b467409763

--- assets/ios.transition-legacy-Cdil3Cjq.js
+++ assets/ios.transition-legacy-Cdil3Cjq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CKCIfUPf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Cf7-fS18.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
