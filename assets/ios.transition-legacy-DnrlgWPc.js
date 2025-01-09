60b655ecbc02799a3b4d07aeba5167340cfa99fe
bertoramos <bertoramos@users.noreply.github.com>
2025-01-09 22:00:04 +0000

    deploy: 0b46537b810d642d60f27875450afd5595ac63f5

--- assets/ios.transition-legacy-DnrlgWPc.js
+++ assets/ios.transition-legacy-DnrlgWPc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DSBiPcoD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CdSO-Hi8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
